61eb7d9600c95fb11cbddf90fc29440d05b2c125
kgilles <kenny.gilles@gmail.com>
2023-12-02 15:38:41 -0500

    Add user page placeholder

---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ParagraphTitle } from '@/components/text';
+
+const User = () => (
+  <div>
+    <ParagraphTitle>User page WIP</ParagraphTitle>
+  </div>
+);
+
+export default User;
