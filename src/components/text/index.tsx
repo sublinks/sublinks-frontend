67a79cf87f6a2418d90280ab8b338bdf7ed235dc
kgilles <kenny.gilles@gmail.com>
2023-12-02 22:21:13 -0500

    Enable title for text components

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -4,13 +4,14 @@ import cx from 'classnames';
 interface TextProps {
   children: string | number;
   className?: string;
+  title?: string;
 }
 
-const BodyTitle = ({ children, className }: TextProps) => <span className={cx('text-sm text-gray-900 dark:text-gray-100', className)}>{children}</span>;
+const BodyTitle = ({ children, className, title }: TextProps) => <span className={cx('text-sm text-gray-900 dark:text-gray-100', className)} title={title}>{children}</span>;
 
-const BodyText = ({ children, className }: TextProps) => <span className={cx('text-xs text-gray-600 dark:text-gray-200', className)}>{children}</span>;
+const BodyText = ({ children, className, title }: TextProps) => <span className={cx('text-xs text-gray-600 dark:text-gray-200', className)} title={title}>{children}</span>;
 
-const PaleBodyText = ({ children, className }: TextProps) => <span className={cx('text-xs text-gray-500 dark:text-gray-400', className)}>{children}</span>;
+const PaleBodyText = ({ children, className, title }: TextProps) => <span className={cx('text-xs text-gray-500 dark:text-gray-400', className)} title={title}>{children}</span>;
 
 export {
   BodyTitle,
