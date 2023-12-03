e0fac28f4ee59f532b4c12d1e5d9f032c8ad23b0
kgilles <kenny.gilles@gmail.com>
2023-12-03 14:13:31 -0500

    Add H1 text

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -7,6 +7,8 @@ interface TextProps {
   title?: string;
 }
 
+const H1 = ({ children, className, title }: TextProps) => <span className={cx('text-lg text-gray-900 dark:text-gray-100', className)} title={title}>{children}</span>;
+
 const BodyTitle = ({ children, className, title }: TextProps) => <span className={cx('text-gray-900 dark:text-gray-100', className)} title={title}>{children}</span>;
 
 const BodyText = ({ children, className, title }: TextProps) => <span className={cx('text-gray-600 dark:text-gray-200', className)} title={title}>{children}</span>;
@@ -16,6 +18,7 @@ const PaleBodyText = ({ children, className, title }: TextProps) => <span classN
 const LinkText = ({ children, className, title }: TextProps) => <span className={cx('text-gray-900 dark:text-gray-100 hover:text-hover-link dark:hover:text-hover-link-dark', className)} title={title}>{children}</span>;
 
 export {
+  H1,
   BodyTitle,
   BodyText,
   PaleBodyText,
