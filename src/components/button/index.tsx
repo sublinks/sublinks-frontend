2451530a912fa0ea35bbbb129e94405b2078e9c7
kgilles <kenny.gilles@gmail.com>
2023-12-09 21:28:03 -0500

    Create button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cx from 'classnames';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  type: 'button' | 'submit' | 'reset';
+  id: string;
+  ariaLabel?: string;
+  className?: string;
+}
+
+const Button = ({
+  ariaLabel, children, className, id, type
+}: ButtonProps) => (
+  // Rule doesn't like type being a variable even though types force it to be a valid option
+  // eslint-disable-next-line react/button-has-type
+  <button type={type} aria-label={ariaLabel} id={id} className={cx('bg-brand-dark dark:bg-brand hover:bg-opacity-90 rounded-md px-23 py-12', className)}>{children}</button>
+);
+
+export default Button;
