e417324b95e0f2bf501792e6f263b2b4d895bac6
Philipp Herz <rooki@rooki.xyz>
2023-12-13 22:45:38 +0100

    Fixed eslint and any type props

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,30 +2,35 @@ import React from 'react';
 
 import { PersonBio, PersonHeader } from '@/components/person-detail';
 
-import * as testData from '../../../test-person-data.json';
 import MainCard from '@/components/main-card';
 import { ModeratesList, ModeratesProps } from '@/components/moderates-list';
 import { PersonDetailSelection } from '@/components/person-comments-posts';
+import * as testData from '../../../test-person-data.json';
 
 const User = () => (
   <div>
-  <div className="mb-12">
-  <MainCard>
-    <div className="flex">
-      <div className="w-8/12 lg:w-9/12 relative mr-16">
-        <PersonHeader avatar={testData.person_view.person.avatar} is_admin={testData.person_view.is_admin} name={testData.person_view.person.display_name} banner={testData.person_view.person.banner} />
-        <PersonBio bio={testData.person_view.person.bio} />
-      </div>
-      <div className="w-4/12 lg:w-3/12 relative">
-        <ModeratesList moderates={testData.moderates as ModeratesProps[]} />
-      </div>
+    <div className="mb-12">
+      <MainCard>
+        <div className="flex">
+          <div className="w-8/12 lg:w-9/12 relative mr-16">
+            <PersonHeader
+              avatar={testData.person_view.person.avatar}
+              is_admin={testData.person_view.is_admin}
+              name={testData.person_view.person.display_name}
+              banner={testData.person_view.person.banner}
+            />
+            <PersonBio bio={testData.person_view.person.bio} />
+          </div>
+          <div className="w-4/12 lg:w-3/12 relative">
+            <ModeratesList moderates={testData.moderates as ModeratesProps[]} />
+          </div>
+        </div>
+      </MainCard>
     </div>
-  </MainCard>
+    <MainCard>
+      <PersonDetailSelection />
+    </MainCard>
   </div>
-  <MainCard>
-    <PersonDetailSelection posts={testData.posts as any} comments={testData.comments as any} />
-  </MainCard>
-  </div>
-  );
+);
 
 export default User;
--- src/components/person-comments-posts/index.tsx
+++ src/components/person-comments-posts/index.tsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import Link from 'next/link';
-import { Components } from 'react-markdown';
 import {
   Tabs,
   TabsHeader,
   TabsBody,
   Tab,
-  TabPanel,
-} from "@/utils/TailwindMaterial";
-
-import { LinkText } from '../text';
+  TabPanel
+} from '@/utils/TailwindMaterial';
 
 const tabs = [
   { label: 'Posts', value: 'posts' },
-  { label: 'Comments', value: 'comments' },
-]
+  { label: 'Comments', value: 'comments' }
+];
 
 // @todo: implement posts and comments
-export const PersonDetailSelection = ({ posts, comments }: { posts: {}[], comments: {}[] }) => 
-(<div className="mt-24 text-gray-600 dark:text-gray-200 text-sm">
-  <Tabs value="posts" >
-    <TabsHeader
-      indicatorProps={{
-        className: "bg-gray-900/10 shadow-none !text-gray-900",
-      }}>
-      {tabs.map(({ label, value }) => (
-        <Tab key={value} value={value}>
-          {label}
-        </Tab>
-      ))}
-    </TabsHeader>
-    <TabsBody>
-      {tabs.map(({ value }) => (
-        <TabPanel key={value} value={value} className="">
-          TODO: {value}
-        </TabPanel>
-      ))}
-    </TabsBody>
-  </Tabs>
-</div>)
+export const PersonDetailSelection = () => (
+  <div className="mt-24 text-gray-600 dark:text-gray-200 text-sm">
+    <Tabs value="posts">
+      <TabsHeader
+        indicatorProps={{
+          className: 'bg-gray-900/10 shadow-none !text-gray-900'
+        }}
+      >
+        {tabs.map(({ label, value }) => (
+          <Tab key={value} value={value}>
+            {label}
+          </Tab>
+        ))}
+      </TabsHeader>
+      <TabsBody>
+        {tabs.map(({ value }) => (
+          <TabPanel key={value} value={value} className="">
+            TODO:
+            {' '}
+            {value}
+          </TabPanel>
+        ))}
+      </TabsBody>
+    </Tabs>
+  </div>
+);
