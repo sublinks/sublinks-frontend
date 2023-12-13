ebcc6b94699296678e93bc1ba5d2334a04fe2f1b
kgilles <kenny.gilles@gmail.com>
2023-12-12 20:21:57 -0500

    Use dynamic client arguments

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,9 +2,11 @@ import { SublinksClient } from 'sublinks-js-client';
 
 let client: SublinksClient;
 
+const { NODE_ENV, SUBLINKS_API_BASE_URL } = process.env;
+
 const sublinksClient = () => {
   if (!client) {
-    client = new SublinksClient('localhost:8080', { insecure: true });
+    client = new SublinksClient(SUBLINKS_API_BASE_URL || 'localhost:8080', { insecure: NODE_ENV !== 'production' });
   }
 
   return client;
