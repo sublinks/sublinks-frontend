c4973111f1ff05dfb56f9b8483c3eeb1554c1763
Philipp Herz <rooki@rooki.xyz>
2023-12-14 20:31:49 +0100

    Added person post feed

--- src/components/person-comments-posts/index.tsx
+++ src/components/person-comments-posts/index.tsx
@@ -6,36 +6,50 @@ import {
   Tab,
   TabPanel
 } from '@/components/TailwindMaterial';
+import { Community, Post, PostAggregates } from 'sublinks-js-client';
+import { PersonPostFeed } from '../person-post-feed';
 
-const tabs = [
-  { label: 'Posts', value: 'posts' },
-  { label: 'Comments', value: 'comments' }
-];
+interface PersonDetailSelectionProps {
+  postViews: {
+    post: Post;
+    counts: PostAggregates;
+    community: Community;
+  }[]
+}
 
 // @todo: implement posts and comments
-export const PersonDetailSelection = () => (
-  <div className="mt-24 text-gray-600 dark:text-gray-200 text-sm">
-    <Tabs value="posts">
-      <TabsHeader
-        indicatorProps={{
-          className: 'bg-gray-900/10 shadow-none !text-gray-900'
-        }}
-      >
-        {tabs.map(({ label, value }) => (
-          <Tab key={value} value={value}>
-            {label}
-          </Tab>
-        ))}
-      </TabsHeader>
-      <TabsBody>
-        {tabs.map(({ value }) => (
-          <TabPanel key={value} value={value}>
-            TODO:
-            {' '}
-            {value}
-          </TabPanel>
-        ))}
-      </TabsBody>
-    </Tabs>
-  </div>
-);
+export const PersonDetailSelection = ({ postViews }: PersonDetailSelectionProps) => {
+  const tabs: {
+    label: string;
+    value: string;
+    element?: React.JSX.Element;
+  }[] = [
+    { label: 'Posts', value: 'posts', element: <PersonPostFeed data={postViews.slice(0, 25)} /> },
+    { label: 'Comments', value: 'comments' }
+  ];
+
+  return (
+    <div className="mt-24 text-gray-600 dark:text-gray-200 text-sm">
+      <Tabs value="posts">
+        <TabsHeader
+          indicatorProps={{
+            className: 'bg-gray-900/10 shadow-none !text-gray-900'
+          }}
+        >
+          {tabs.map(({ label, value }) => (
+            <Tab key={value} value={value}>
+              {label}
+            </Tab>
+          ))}
+        </TabsHeader>
+        <TabsBody>
+          {tabs.map(({ value, element }) => (
+            <TabPanel key={value} value={value} className="mt-8">
+              {element || `TODO: ${value}`}
+            </TabPanel>
+          ))}
+        </TabsBody>
+      </Tabs>
+    </div>
+  );
+};
