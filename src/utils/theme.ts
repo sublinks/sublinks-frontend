4ce94c6f7bedd6a62e9e81646e4866f816cfbcac
Philipp Herz <rooki@rooki.xyz>
2023-12-15 21:42:28 +0100

    lint

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -2,7 +2,6 @@
 
 import { useLocalStorage } from './localstorage';
 
-
 export function useTheme() {
   return useLocalStorage('theme', typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 }
