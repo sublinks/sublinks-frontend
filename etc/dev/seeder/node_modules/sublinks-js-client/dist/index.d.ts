f10cf3c5f430bd8ebc3f66305bc973eb8fecb195
kgilles <kenny.gilles@gmail.com>
2023-12-16 13:06:19 -0500

    Remove modules

--- etc/dev/seeder/node_modules/sublinks-js-client/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { HeadersObject } from './types/HeadersObject';
-export { HttpClientConstructorOptions } from './types/HttpClientConstructorOptions';
-export { StatusResponse } from './types/StatusResponse';
-export * from 'lemmy-js-client';
-export * from './client.js';
-export * from './native-client';
--- etc/dev/seeder/node_modules/sublinks-js-client/dist/types/StatusResponse.d.ts
+++
@@ -1,4 +0,0 @@
-export interface StatusResponse {
-    success: boolean;
-    message?: string;
-}
