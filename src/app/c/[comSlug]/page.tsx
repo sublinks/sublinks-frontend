0c17207a96d288c30551e9e384ca806c6ac926fb
kgilles <kenny.gilles@gmail.com>
2023-12-23 17:21:12 -0500

    Add not found state for community page

--- src/app/c/[comSlug]/page.tsx
+++ src/app/c/[comSlug]/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { notFound } from 'next/navigation';
 
 import PostFeed from '@/components/post-feed';
+import { H1 } from '@/components/text';
 import sublinksClient from '@/utils/client';
 import { getCommunityNameFromSlug } from '@/utils/communities';
 
@@ -12,18 +14,28 @@ interface CommunityFeedProps {
 }
 
 const CommunityFeed = async ({ params: { comSlug } }: CommunityFeedProps) => {
-  const communityName = getCommunityNameFromSlug(comSlug);
-  const postsRes = await sublinksClient().getPosts({
-    community_name: communityName,
-    type_: 'All',
-    sort: 'Active'
-  });
+  try {
+    const communityName = getCommunityNameFromSlug(comSlug);
+    const communityData = await sublinksClient().getCommunity({
+      name: communityName
+    });
 
-  return (
-    <div>
-      <PostFeed data={postsRes.posts} isCommunityFeed />
-    </div>
-  );
+    const communityPostsData = await sublinksClient().getPosts({
+      community_name: communityName,
+      type_: 'All',
+      sort: 'Active'
+    });
+    const { title } = communityData.community_view.community;
+
+    return (
+      <div>
+        <H1 className="ml-8">{title}</H1>
+        <PostFeed data={communityPostsData.posts} isCommunityFeed />
+      </div>
+    );
+  } catch (e) {
+    return notFound();
+  }
 };
 
 export default CommunityFeed;
