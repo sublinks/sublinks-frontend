e3273780d643f84ed85e0cf8d2f2e5717db8b8a9
kgilles <kenny.gilles@gmail.com>
2023-12-23 16:02:09 -0500

    Configure next images for local

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "http",
+        hostname: "sublinks"
+      },
+      {
         protocol: "https",
         hostname: "**",
       }
