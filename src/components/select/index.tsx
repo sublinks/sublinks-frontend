1bc91f149b7788f0c418f8eab2888bc630b9e7db
rooki <rooki@rooki.xyz>
2024-01-05 13:15:04 +0100

    fixed eslint

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -3,22 +3,28 @@
 import React from 'react';
 import cx from 'classnames';
 
-interface SortProps extends
-  React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement> {
+interface SortProps {
+  className?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  children: React.ReactNode;
 }
 
-const Select = ({ className, children, ...rest }: SortProps) => (
-  <select {...rest} className={cx('h-full rounded-md bg-gray-200 dark:bg-gray-400 pl-4', className)}>
+const Select = ({
+  className, value, onChange, children
+}: SortProps) => (
+  <select onChange={onChange} value={value} className={cx('h-full rounded-md bg-gray-200 dark:bg-gray-400 pl-4', className)}>
     {children}
   </select>
 );
 
-interface OptionProps extends
-  React.DetailedHTMLProps<React.OptionHTMLAttributes<HTMLOptionElement>, HTMLOptionElement> {
+interface OptionProps {
+  value?: string;
+  children: React.ReactNode;
 }
 
-const Option = ({ children, ...rest }: OptionProps) => (
-  <option {...rest}>
+const Option = ({ children, value }: OptionProps) => (
+  <option value={value}>
     {children}
   </option>
 );
