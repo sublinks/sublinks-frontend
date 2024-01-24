209635c288a7fcebd8de0123d973dd514a09c3ce
Ategon <benjamin@barbeau.net>
2024-01-24 03:32:42 -0500

    Only enable navbar hiding on certain pages

--- src/components/bottom-nav/bottom-nav-div.tsx
+++ src/components/bottom-nav/bottom-nav-div.tsx
@@ -2,10 +2,13 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import cx from 'classnames';
+import { usePathname } from 'next/navigation';
 
 const BottomNavDiv = ({ children }: { children: React.ReactNode }) => {
   const [wentUp, setWentUp] = useState(true);
+  const [navHidingEnabled, setNavHidingEnabled] = useState(true);
   const lastScrollY = useRef(0);
+  const path = usePathname();
 
   function handleScroll() {
     const currentScrollY = window.scrollY;
@@ -20,6 +23,15 @@ const BottomNavDiv = ({ children }: { children: React.ReactNode }) => {
   }
 
   useEffect(() => {
+    const regex = /\/c\/[^/]+(?!.)/; // Community URL
+    if (path === '/' || regex.test(path)) {
+      setNavHidingEnabled(true);
+    } else {
+      setNavHidingEnabled(false);
+    }
+  }, [path]);
+
+  useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -27,7 +39,7 @@ const BottomNavDiv = ({ children }: { children: React.ReactNode }) => {
   }, []);
 
   return (
-    <div className={cx('bottom-0 w-full h-48 flex items-center justify-around py-8 px-8 md:px-16 border-t bg-white z-10 dark:bg-black md:hidden backdrop-blur-lg border-gray-200 dark:border-gray-400 bg-opacity-60 dark:bg-opacity-60 fixed transition-all duration-300', wentUp ? 'bottom-0' : '-bottom-48')}>
+    <div className={cx('w-full h-48 flex items-center justify-around py-8 px-8 md:px-16 border-t bg-white z-10 dark:bg-black md:hidden backdrop-blur-lg border-gray-200 dark:border-gray-400 bg-opacity-60 dark:bg-opacity-60 fixed transition-all duration-300', (wentUp || !navHidingEnabled) ? 'bottom-0' : '-bottom-48')}>
       { children }
     </div>
   );
