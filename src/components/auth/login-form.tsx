5a1c02efb5c6ce4c547c5f0d92c345ed0da30576
kgilles <kenny.gilles@gmail.com>
2024-01-26 23:00:14 -0500

    Add login handler

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,18 +1,57 @@
 'use client';
 
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 import { InputField } from '@/components/input';
 import Button from '@/components/button';
+import sublinksClient from '@/utils/client';
+import { BodyTitleInverse, ErrorText } from '../text';
 
-const LoginForm = () => (
-  <div className="flex flex-col">
-    <div className="flex flex-col gap-16 mb-32">
-      <InputField type="text" label="Username" name="username" id="username" placeholder="Username" showBorderPlaceholder />
-      <InputField type="password" label="Password" name="password" id="password" placeholder="Password" showBorderPlaceholder />
-    </div>
-    <Button type="submit">Login</Button>
-  </div>
-);
+const LoginForm = () => {
+  const [error, setError] = useState<string>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleLoginAttempt = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    setError(undefined);
+
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get('username') as string;
+    const password = formData.get('password') as string;
+
+    if (!username || !password) {
+      setError('Please enter your username and password');
+      setIsSubmitting(false);
+      return;
+    }
+
+    try {
+      await sublinksClient().login({
+        username_or_email: username,
+        password
+      });
+    } catch (e) {
+      setError('Login attempt failed. Please try again.');
+    }
+
+    setIsSubmitting(false);
+  };
+
+  return (
+    <form onSubmit={handleLoginAttempt} className="flex flex-col">
+      <div className="flex flex-col gap-16">
+        <InputField type="text" label="Username" name="username" id="username" placeholder="Username" showBorderPlaceholder disabled={isSubmitting} />
+        <InputField type="password" label="Password" name="password" id="password" placeholder="Password" showBorderPlaceholder disabled={isSubmitting} />
+      </div>
+      <div className="h-32">
+        {error && <ErrorText className="text-sm">{error}</ErrorText>}
+      </div>
+      <Button type="submit" disabled={isSubmitting}>
+        <BodyTitleInverse>Log In</BodyTitleInverse>
+      </Button>
+    </form>
+  );
+};
 
 export default LoginForm;
