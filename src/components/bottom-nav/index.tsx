6ea8574c7ec7377a27a1b42156bbf74752b7b521
kgilles <kenny.gilles@gmail.com>
2024-02-05 23:20:49 -0500

    Pass user data to mobile nav

--- src/components/bottom-nav/index.tsx
+++ src/components/bottom-nav/index.tsx
@@ -3,27 +3,37 @@ import Link from 'next/link';
 import {
   DocumentPlusIcon, MagnifyingGlassIcon, UserGroupIcon
 } from '@heroicons/react/24/outline';
+import { GetSiteResponse } from 'sublinks-js-client';
 
+import SublinksApi from '@/utils/api-client/client';
 import Icon, { ICON_SIZE } from '../icon';
-import ProfileMenu from '../user-nav';
+import UserNav from '../user-nav';
 import BottomNavDiv from './bottom-nav-div';
+import * as testData from '../../../test-instance-data.json';
 
-const BottomNav = () => (
-  <BottomNavDiv>
-    <Link href="/communities">
-      <Icon IconType={UserGroupIcon} size={ICON_SIZE.MEDIUM} title="Communities icon" isInteractable />
-    </Link>
+const BottomNav = async () => {
+  const instance = process.env.NEXT_PUBLIC_SUBLINKS_API_BASE_URL
+    ? await SublinksApi.Instance().Client().getSite()
+    : testData as unknown as GetSiteResponse;
+  const myUser = instance?.my_user;
 
-    <Link href="/p">
-      <Icon IconType={DocumentPlusIcon} size={ICON_SIZE.MEDIUM} title="Create post icon" isInteractable />
-    </Link>
+  return (
+    <BottomNavDiv>
+      <Link href="/communities">
+        <Icon IconType={UserGroupIcon} size={ICON_SIZE.MEDIUM} title="Communities icon" isInteractable />
+      </Link>
 
-    <button type="button" aria-label="Search">
-      <Icon IconType={MagnifyingGlassIcon} size={ICON_SIZE.MEDIUM} title="Search icon" isInteractable />
-    </button>
+      <Link href="/p">
+        <Icon IconType={DocumentPlusIcon} size={ICON_SIZE.MEDIUM} title="Create post icon" isInteractable />
+      </Link>
 
-    <ProfileMenu />
-  </BottomNavDiv>
-);
+      <button type="button" aria-label="Search">
+        <Icon IconType={MagnifyingGlassIcon} size={ICON_SIZE.MEDIUM} title="Search icon" isInteractable />
+      </button>
+
+      <UserNav initialMyUser={myUser} />
+    </BottomNavDiv>
+  );
+};
 
 export default BottomNav;
