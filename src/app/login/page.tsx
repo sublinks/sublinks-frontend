36c049eb474fb46dd5ede1f01ec481bf2b08f5ab
kgilles <kenny.gilles@gmail.com>
2024-02-07 20:12:07 -0500

    Center form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,15 +5,17 @@ import Link from 'next/link';
 import LoginForm from '@/components/auth/login-form';
 
 const Login = () => (
-  <div className="flex flex-col p-24 md:p-56 max-w-full md:max-w-500">
-    <H1>Login</H1>
-    <div className="text-sm mt-12 mb-24">
-      <BodyText>{'Don’t have an account? '}</BodyText>
-      <Link href="/signup">
-        <LinkText>Sign up</LinkText>
-      </Link>
+  <div className="flex flex-col items-center p-24 md:p-56 w-full">
+    <div className="w-500">
+      <H1>Login</H1>
+      <div className="text-sm mt-12 mb-24">
+        <BodyText>{'Don’t have an account? '}</BodyText>
+        <Link href="/signup">
+          <LinkText>Sign up</LinkText>
+        </Link>
+      </div>
+      <LoginForm />
     </div>
-    <LoginForm />
   </div>
 );
 
