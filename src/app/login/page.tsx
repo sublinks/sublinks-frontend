5024822d918467d9702b5319ba87f09f8ccbc2fa
kgilles <kenny.gilles@gmail.com>
2024-02-21 21:25:12 -0500

    Responsive login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ import LoginForm from '@/components/auth/login-form';
 
 const Login = () => (
   <div className="flex flex-col items-center p-24 md:p-56 w-full">
-    <div className="w-500">
+    <div className="w-full md:w-500">
       <H1>Login</H1>
       <div className="text-sm mt-12 mb-24">
         <BodyText>{'Don’t have an account? '}</BodyText>
