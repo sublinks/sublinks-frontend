cd21b0e80371afebcdf981809cae233a1f7850ed
kgilles <kenny.gilles@gmail.com>
2024-02-21 20:34:23 -0500

    Add todo for signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { BodyTitle } from '@/components/text';
 
+// @todo: Create signup page
+// https://github.com/sublinks/sublinks-frontend/issues/77
 const Signup = () => (
   <div>
     <BodyTitle>Signup page WIP</BodyTitle>
