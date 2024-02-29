2b6322c36153955a60289b57683429a9c9bc61ce
kgilles <kenny.gilles@gmail.com>
2024-02-28 19:42:18 -0500

    Add error handling to post page

--- src/app/c/[comSlug]/[postId]/page.tsx
+++ src/app/c/[comSlug]/[postId]/page.tsx
@@ -5,8 +5,10 @@ import PostHeader from '@/components/post-header';
 import LinkedPostSubTitle from '@/components/post-subtitle';
 import { getPostThumbnailUrl, isImage } from '@/utils/links';
 import SublinksApi from '@/utils/api-client/server';
+import logger from '@/utils/logger';
 
 import * as testData from '../../../../../test-data.json';
+import { ErrorText } from '@/components/text';
 
 interface PostViewProps {
   params: {
@@ -15,17 +17,28 @@ interface PostViewProps {
   }
 }
 
+const getPost = async (postIdInt: number) => {
+  try {
+    const post = process.env.NEXT_PUBLIC_SUBLINKS_API_BASE_URL
+      ? await SublinksApi.Instance().Client().getPost({
+        id: postIdInt
+      }) : { post_view: testData.posts.find(post => post.post.id === postIdInt)! };
+
+    return post;
+  } catch (e) {
+    logger.error('Failed to retrieve post', e);
+    return undefined;
+  }
+}
+
 const PostView = async ({ params: { postId } }: PostViewProps) => {
   // @todo: Allow test data when in non-docker dev env
   // as Sublinks Core doesn't yet handle all post features
   const postIdInt = parseInt(postId, 10);
-  const postData = process.env.NEXT_PUBLIC_SUBLINKS_API_BASE_URL
-    ? await SublinksApi.Instance().Client().getPost({
-      id: postIdInt
-    }) : { post_view: testData.posts.find(post => post.post.id === postIdInt)! };
+  const postData = await getPost(postIdInt);
 
   if (!postData) {
-    return null;
+    return <ErrorText>Unable to show post. Please reload the page to try again.</ErrorText>;
   }
 
   const { post_view: postView } = postData;
