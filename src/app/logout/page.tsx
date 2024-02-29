5a98dba679a57c47b1d76ea32a96120e9ca133d4
kgilles <kenny.gilles@gmail.com>
2024-02-28 19:51:13 -0500

    Add logout loading state

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/navigation';
 
 import SublinksApi from '@/utils/api-client/client';
 import { UserContext } from '@/context/user';
+import { Spinner } from '@material-tailwind/react';
+import { BodyTitle } from '@/components/text';
 
 const Logout = () => {
   const router = useRouter();
@@ -20,7 +22,12 @@ const Logout = () => {
     logout();
   }, [clearMyUser, router]);
 
-  return <div />;
+  return (
+    <div className='w-full mt-56 flex flex-col gap-8 items-center justify-center'>
+      <BodyTitle>Logging out...</BodyTitle>
+      <Spinner className="h-48 w-48" />
+    </div>
+  );
 };
 
 export default Logout;
