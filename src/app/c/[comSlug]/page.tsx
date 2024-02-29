7607383b23e7c866fe0854e419f6d52ab0f5cf91
kgilles <kenny.gilles@gmail.com>
2024-02-28 19:40:22 -0500

    Add error handling to community page

--- src/app/c/[comSlug]/page.tsx
+++ src/app/c/[comSlug]/page.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { notFound } from 'next/navigation';
 
 import PostFeed from '@/components/post-feed';
-import { H1 } from '@/components/text';
+import { ErrorText, H1 } from '@/components/text';
 import SublinksApi from '@/utils/api-client/server';
 import { getCommunityNameFromSlug } from '@/utils/communities';
+import logger from '@/utils/logger';
 
 interface CommunityFeedProps {
   params: {
@@ -13,24 +14,50 @@ interface CommunityFeedProps {
   }
 }
 
-const CommunityFeed = async ({ params: { comSlug } }: CommunityFeedProps) => {
+const getCommunity = async (communityName: string) => {
   try {
-    const communityName = getCommunityNameFromSlug(comSlug);
-    const communityData = await SublinksApi.Instance().Client().getCommunity({
+    const community = await SublinksApi.Instance().Client().getCommunity({
       name: communityName
     });
 
-    const communityPostsData = await SublinksApi.Instance().Client().getPosts({
+    return community;
+  } catch (e) {
+    logger.error('Failed to retrieve community', e);
+    return undefined;
+  }
+};
+
+const getPosts = async (communityName: string) => {
+  try {
+    const communityPosts = await SublinksApi.Instance().Client().getPosts({
       community_name: communityName,
       type_: 'All',
       sort: 'Active'
     });
-    const { title } = communityData.community_view.community;
+
+    return communityPosts;
+  } catch (e) {
+    logger.error('Failed to retrieve community posts', e);
+    return undefined;
+  }
+};
+
+const CommunityFeed = async ({ params: { comSlug } }: CommunityFeedProps) => {
+  try {
+    const communityName = getCommunityNameFromSlug(comSlug);
+    const community = await getCommunity(communityName);
+    const communityPosts = await getPosts(communityName);
+
+    if (!community || !communityPosts) {
+      return <ErrorText>Unable to show community. Please reload the page to try again.</ErrorText>;
+    }
+
+    const { title } = community.community_view.community;
 
     return (
       <div>
         <H1 className="ml-8">{title}</H1>
-        <PostFeed data={communityPostsData.posts} isCommunityFeed />
+        <PostFeed data={communityPosts.posts} isCommunityFeed />
       </div>
     );
   } catch (e) {
