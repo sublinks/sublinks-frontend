c08229526c22be8d2f15d459a59b66d2d6349fd6
kgilles <kenny.gilles@gmail.com>
2024-02-28 20:15:01 -0500

    Add cookie error handling

--- src/utils/api-client/client.ts
+++ src/utils/api-client/client.ts
@@ -1,5 +1,7 @@
 import Cookies from 'js-cookie';
+
 import SublinksApiBase, { AUTH_COOKIE_NAME, CookieStore } from './base';
+import logger from '../logger';
 
 class SublinksApiClientSide extends SublinksApiBase {
   private static instance: SublinksApiClientSide;
@@ -8,9 +10,28 @@ class SublinksApiClientSide extends SublinksApiBase {
     super();
 
     const cookieStore = {
-      get: () => Cookies.get(AUTH_COOKIE_NAME),
-      set: (value: string, options?: object) => Cookies.set(AUTH_COOKIE_NAME, value, options),
-      remove: () => Cookies.remove(AUTH_COOKIE_NAME)
+      get: () => {
+        try {
+          return Cookies.get(AUTH_COOKIE_NAME);
+        } catch (e) {
+          logger.error('Failed to get client auth cookie', e);
+          return undefined;
+        }
+      },
+      set: (value: string, options?: object) => {
+        try {
+          Cookies.set(AUTH_COOKIE_NAME, value, options);
+        } catch (e) {
+          logger.error('Failed to set client auth cookie', e);
+        }
+      },
+      remove: () => {
+        try {
+          Cookies.remove(AUTH_COOKIE_NAME);
+        } catch (e) {
+          logger.error('Failed to remove client auth cookie', e);
+        }
+      }
     } as CookieStore;
 
     this.setAuthCookieStore(cookieStore);
--- src/utils/api-client/server.ts
+++ src/utils/api-client/server.ts
@@ -1,5 +1,7 @@
 import { cookies } from 'next/headers';
+
 import SublinksApiBase, { AUTH_COOKIE_NAME, CookieStore } from './base';
+import logger from '../logger';
 
 class SublinksApiServerSide extends SublinksApiBase {
   private static instance: SublinksApiServerSide;
@@ -9,12 +11,31 @@ class SublinksApiServerSide extends SublinksApiBase {
 
     const nextCookies = cookies();
     const cookieStore = {
-      get: () => nextCookies.get(AUTH_COOKIE_NAME)?.value,
-      set: (value: string, options?: object) => nextCookies.set(AUTH_COOKIE_NAME, value, {
-        ...options,
-        httpOnly: true
-      }),
-      remove: () => nextCookies.delete(AUTH_COOKIE_NAME)
+      get: () => {
+        try {
+          return nextCookies.get(AUTH_COOKIE_NAME)?.value;
+        } catch (e) {
+          logger.error('Failed to get server auth cookie', e);
+          return undefined;
+        }
+      },
+      set: (value: string, options?: object) => {
+        try {
+          nextCookies.set(AUTH_COOKIE_NAME, value, {
+            ...options,
+            httpOnly: true
+          });
+        } catch (e) {
+          logger.error('Failed to set server auth cookie', e);
+        }
+      },
+      remove: () => {
+        try {
+          nextCookies.delete(AUTH_COOKIE_NAME);
+        } catch (e) {
+          logger.error('Failed to remove server auth cookie', e);
+        }
+      }
     } as CookieStore;
 
     this.setAuthCookieStore(cookieStore);
