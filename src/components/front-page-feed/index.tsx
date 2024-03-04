a23f0cf550d97c68d4386fe9ec92d730ac305013
Evan Johnson <evan@evan.app>
2024-03-04 12:21:31 -0600

    adhere to lint rules
    3:38  Error: 'useRef' is defined but never used.  @typescript-eslint/no-unused-vars
    23:53  Error: Trailing spaces not allowed.  no-trailing-spaces
    25:65  Error: Strings must use singlequote.  @typescript-eslint/quotes
    26:62  Error: Strings must use singlequote.  @typescript-eslint/quotes
    46:1  Error: Trailing spaces not allowed.  no-trailing-spaces
    56:9  Error: Expected space(s) after "catch".  @typescript-eslint/keyword-spacing
    57:61  Error: Trailing spaces not allowed.  no-trailing-spaces
    61:6  Error: React Hook useEffect has missing dependencies: 'initialPostFeedSort' and 'initialPostFeedType'. Either include them or remove the dependency array.  react-hooks/exhaustive-deps

--- src/components/front-page-feed/index.tsx
+++ src/components/front-page-feed/index.tsx
@@ -20,15 +20,15 @@ interface FeedProps {
 const Feed = ({ posts, site }: FeedProps) => {
   const [postFeed, setPostFeed] = useState<GetPostsResponse>(posts);
 
-  // @todo: Set this to the users default feed type, 
+  // @todo: Set this to the users default feed type,
   // temporarily setting default values to track initial state
-  const [postFeedType, setPostFeedType] = useState<ListingType>("All");
-  const [postFeedSort, setPostFeedSort] = useState<SortType>("Hot");
+  const [postFeedType, setPostFeedType] = useState<ListingType>('All');
+  const [postFeedSort, setPostFeedSort] = useState<SortType>('Hot');
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
 
   // State to store the initial values of postFeedType and postFeedSort
-  const [initialPostFeedType, setInitialPostFeedType] = useState<ListingType>(postFeedType);
-  const [initialPostFeedSort, setInitialPostFeedSort] = useState<SortType>(postFeedSort);
+  const initialPostFeedTypeRef = useRef<ListingType>(postFeedType);
+  const initialPostFeedSortRef = useRef<SortType>(postFeedSort);
 
   const handleSidebarSwitch = (newState: boolean) => {
     setSidebarOpen(newState);
@@ -43,19 +43,21 @@ const Feed = ({ posts, site }: FeedProps) => {
         sort: postFeedSort
       }) : testData as unknown as GetPostsResponse);
     }
-    
+
+    const isPostFeedTypeChanged = postFeedType !== initialPostFeedTypeRef.current;
+    const isPostFeedSortChanged = postFeedSort !== initialPostFeedSortRef.current;
+
     // Only call getPosts if postFeedType or postFeedSort has changed from its initial value
-    if (postFeedType !== initialPostFeedType || postFeedSort !== initialPostFeedSort) {
+    if (isPostFeedTypeChanged || isPostFeedSortChanged) {
       // @todo: indication posts are loading
       try {
         getPosts();
 
         // Update initial values to reflect the current state
-        setInitialPostFeedType(postFeedType);
-        setInitialPostFeedSort(postFeedSort);
-      } catch(e) {
-        // @todo: add error handling re: src/utils/logger.ts 
-        // https://github.com/sublinks/sublinks-frontend/pull/79/commits/935bf15b1c4a5a1c3a5b1b42a4af0e291118b071
+        initialPostFeedTypeRef.current = postFeedType;
+        initialPostFeedSortRef.current = postFeedSort;
+      } catch (e) {
+        // @todo: add error handling re: src/utils/logger.ts
       }
     }
   }, [postFeedSort, postFeedType]);
