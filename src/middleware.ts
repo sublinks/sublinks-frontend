eb436f25f2acbb41eb03802a3c302d9c1ce8fc9f
kgilles <kenny.gilles@gmail.com>
2024-03-06 19:52:21 -0500

    Update sublinks client

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server';
-import fetch from 'cross-fetch';
+import SublinksApi from '@/utils/api-client/server';
 
 export const middleware = async () => {
   // Check auth header for server
 
-  const site = await fetch('http://sublinks:8080/api/v3/site');
-  console.log(await site.json());
+  const site = await SublinksApi.Instance().Client().getSite();
+  console.log(site);
 
   return NextResponse.next();
 };
