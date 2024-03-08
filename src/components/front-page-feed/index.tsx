9a9ff4a942873b82410e40d5d830a2b84a53bda8
Evan <slickplaid@users.noreply.github.com>
2024-03-07 21:06:10 -0600

    Prevent initial useEffect call in Feed component (#78)
    * Prevent initial useEffect call in Feed component
    
    Modified the Feed component to prevent the useEffect hook from running its logic on the initial component mount. Introduced a useRef hook to track the component's mount status. This change ensures that the getPosts function is only called in response to changes in postFeedSort or postFeedType, avoiding unnecessary API calls during the initial render.
    
    https://github.com/sublinks/sublinks-frontend/issues/48
    
    * only load posts if dependency state changes
    
    * adhere to lint rules
    
    3:38  Error: 'useRef' is defined but never used.  @typescript-eslint/no-unused-vars
    23:53  Error: Trailing spaces not allowed.  no-trailing-spaces
    25:65  Error: Strings must use singlequote.  @typescript-eslint/quotes
    26:62  Error: Strings must use singlequote.  @typescript-eslint/quotes
    46:1  Error: Trailing spaces not allowed.  no-trailing-spaces
    56:9  Error: Expected space(s) after "catch".  @typescript-eslint/keyword-spacing
    57:61  Error: Trailing spaces not allowed.  no-trailing-spaces
    61:6  Error: React Hook useEffect has missing dependencies: 'initialPostFeedSort' and 'initialPostFeedType'. Either include them or remove the dependency array.  react-hooks/exhaustive-deps
    
    ---------
    
    Co-authored-by: kgilles <kenny.gilles@gmail.com>

--- src/components/front-page-feed/index.tsx
+++ src/components/front-page-feed/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import PostFeed from '@/components/post-feed';
 import sublinksClient from '@/utils/client';
@@ -20,11 +20,15 @@ interface FeedProps {
 const Feed = ({ posts, site }: FeedProps) => {
   const [postFeed, setPostFeed] = useState<GetPostsResponse>(posts);
 
-  // @todo: Set this to the users default feed type
-  const [postFeedType, setPostFeedType] = useState<ListingType>();
-  const [postFeedSort, setPostFeedSort] = useState<SortType>();
-
+  // @todo: Set this to the users default feed type,
+  // temporarily setting default values to track initial state
+  const [postFeedType, setPostFeedType] = useState<ListingType>('All');
+  const [postFeedSort, setPostFeedSort] = useState<SortType>('Hot');
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+
+  // State to store the initial values of postFeedType and postFeedSort
+  const initialPostFeedTypeRef = useRef<ListingType>(postFeedType);
+  const initialPostFeedSortRef = useRef<SortType>(postFeedSort);
 
   const handleSidebarSwitch = (newState: boolean) => {
     setSidebarOpen(newState);
@@ -39,7 +43,23 @@ const Feed = ({ posts, site }: FeedProps) => {
         sort: postFeedSort
       }) : testData as unknown as GetPostsResponse);
     }
-    getPosts();
+
+    const isPostFeedTypeChanged = postFeedType !== initialPostFeedTypeRef.current;
+    const isPostFeedSortChanged = postFeedSort !== initialPostFeedSortRef.current;
+
+    // Only call getPosts if postFeedType or postFeedSort has changed from its initial value
+    if (isPostFeedTypeChanged || isPostFeedSortChanged) {
+      // @todo: indication posts are loading
+      try {
+        getPosts();
+
+        // Update initial values to reflect the current state
+        initialPostFeedTypeRef.current = postFeedType;
+        initialPostFeedSortRef.current = postFeedSort;
+      } catch (e) {
+        // @todo: add error handling re: src/utils/logger.ts
+      }
+    }
   }, [postFeedSort, postFeedType]);
 
   return (
