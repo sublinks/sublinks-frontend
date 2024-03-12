b39230193c2ba51b146f18d36f30ae560e5ec6f1
Ategon <benjamin@barbeau.net>
2024-03-12 00:22:44 -0400

    Add tabs

--- src/components/person-comments-posts/index.tsx
+++ src/components/person-comments-posts/index.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import React from 'react';
 import { CommentView, PostView } from 'sublinks-js-client';
 import PostFeed from '../post-feed';
 import CommentFeed from '../comment-feed';
+import { Tab, Tabs } from '@nextui-org/tabs';
 
 interface PersonDetailSelectionProps {
   postViews: PostView[],
@@ -20,60 +23,13 @@ export const PersonDetailSelection = ({ postViews, commentViews }: PersonDetailS
 
   return (
     <div className="mt-8 text-sm">
-      
+      <Tabs aria-label="Posts" items={tabs}>
+        {({value, label, content}) => (
+          <Tab key={value} title={label}>
+            {content}
+          </Tab>
+        )}
+      </Tabs>
     </div>
   );
 };
-
-/*
-<Tabs aria-label="Posts"></Tabs>
-
-
-<TabsHeader
-          indicatorProps={{
-            className: "bg-gray-900/10 dark:bg-secondary-dark shadow-none",
-          }}
-          className="bg-secondary dark:bg-gray-900/10"
-        >
-          {tabs.map(({ label, value }) => (
-            <Tab
-              key={value}
-              value={value}
-              className="text-black dark:text-white"
-            >
-              {label}
-            </Tab>
-          ))}
-        </TabsHeader>
-        <TabsBody>
-          {tabs.map(({ value, content }) => (
-            <TabPanel key={value} value={value} className="mt-8">
-              {content || `TODO: ${value}`}
-            </TabPanel>
-          ))}
-        </TabsBody>
-*/
-
-/*
-<Tabs value="posts">
-        <TabsHeader
-          indicatorProps={{
-            className: 'bg-gray-900/10 dark:bg-secondary-dark shadow-none'
-          }}
-          className="bg-secondary dark:bg-gray-900/10"
-        >
-          {tabs.map(({ label, value }) => (
-            <Tab key={value} value={value} className="text-black dark:text-white">
-              {label}
-            </Tab>
-          ))}
-        </TabsHeader>
-        <TabsBody>
-          {tabs.map(({ value, content }) => (
-            <TabPanel key={value} value={value} className="mt-8">
-              {content || `TODO: ${value}`}
-            </TabPanel>
-          ))}
-        </TabsBody>
-      </Tabs>
-*/
