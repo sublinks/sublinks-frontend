aa2e5abda4faa5e2aea75ff09afffdb13d4c7d43
Kenny Gilles <kenny.gilles@gmail.com>
2024-03-14 11:46:34 -0400

    Add initial test setup

---
+++ tests/unit/utils/communities.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+
+describe('communities', () => {
+  it('should return a thing', () => {
+    assert.equal(true, true);
+  });
+});
