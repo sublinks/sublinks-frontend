8431b476720d561ceb99bd3ac7af656adfad831c
Kenny Gilles <kenny.gilles@gmail.com>
2024-03-14 14:09:06 -0400

    Replace node runner with jest

--- tests/unit/utils/communities.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'node:test';
-import assert from 'node:assert';
-
-describe('communities', () => {
-  it('should return a thing', () => {
-    assert.equal(true, true);
-  });
-});
