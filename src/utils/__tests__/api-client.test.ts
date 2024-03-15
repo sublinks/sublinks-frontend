191b77550aab6850628cb43f820a9d23f441a738
Kenny Gilles <kenny.gilles@gmail.com>
2024-03-15 17:22:43 -0400

    Add more base tests

--- src/utils/__tests__/api-client.test.ts
+++ src/utils/__tests__/api-client.test.ts
@@ -1,15 +1,24 @@
-import { LoginResponse, SublinksClient } from 'sublinks-js-client';
+import { GetSiteResponse, LoginResponse, SublinksClient } from 'sublinks-js-client';
 
 import SublinksApiBase from '../api-client/base';
 import logger from '../logger';
 
-beforeAll(() => {
+let errorSpy: jest.SpyInstance;
+
+beforeEach(() => {
+  jest.spyOn(SublinksClient.prototype, 'getSite').mockResolvedValue({
+    my_user: {}
+  } as GetSiteResponse);
   jest.spyOn(SublinksClient.prototype, 'login').mockResolvedValue({
     jwt: 'test-jwt-token'
   } as LoginResponse);
+  jest.spyOn(SublinksClient.prototype, 'getPosts').mockRejectedValue(new Error('Unauthorized'));
+
+  errorSpy = jest.spyOn(logger, 'error').mockImplementation(() => {});
+  jest.spyOn(logger, 'debug').mockImplementation(() => {});
 });
 
-afterAll(() => jest.clearAllMocks());
+afterEach(() => jest.clearAllMocks());
 
 describe.only('base class', () => {
   it('creates the base class with expected public properties', () => {
@@ -37,7 +46,6 @@ describe.only('base class', () => {
   });
 
   it('can set auth header', () => {
-    const errorSpy = jest.spyOn(logger, 'error').mockImplementation(() => {});
     const apiClient = new SublinksApiBase();
 
     apiClient.setAuthHeader('test-jwt-token');
@@ -69,6 +77,52 @@ describe.only('base class', () => {
     expect(cookieStore.set).toHaveBeenCalled();
   });
 
+  it('throws error if API login request fails', async () => {
+    jest.clearAllMocks();
+    expect.assertions(1);
+
+    jest.spyOn(SublinksClient.prototype, 'login').mockRejectedValue(new Error('request failed'));
+
+    const apiClient = new SublinksApiBase();
+    const cookieStore = {
+      get: jest.fn(),
+      set: jest.fn().mockImplementation(() => {
+        throw new Error('cookie set failed');
+      }),
+      remove: jest.fn()
+    };
+
+    apiClient.setAuthCookieStore(cookieStore);
+
+    try {
+      await apiClient.login('bill', 'password123');
+    } catch (e) {
+      const error = e as Error;
+      expect(error.message).toBe('request failed');
+    }
+  });
+
+  it('throws error if failing to set auth cookie', async () => {
+    expect.assertions(1);
+    const apiClient = new SublinksApiBase();
+    const cookieStore = {
+      get: jest.fn(),
+      set: jest.fn().mockImplementation(() => {
+        throw new Error('cookie set failed');
+      }),
+      remove: jest.fn()
+    };
+
+    apiClient.setAuthCookieStore(cookieStore);
+
+    try {
+      await apiClient.login('bill', 'password123');
+    } catch (e) {
+      const error = e as Error;
+      expect(error.message).toBe('cookie set failed');
+    }
+  });
+
   it('clears authorization on logout', () => {
     const apiClient = new SublinksApiBase();
     const cookieStore = {
@@ -81,5 +135,78 @@ describe.only('base class', () => {
     apiClient.logout();
 
     expect(cookieStore.remove).toHaveBeenCalled();
+  });
+
+  describe('wrapped client', () => {
+    it('wraps the API client function properties', async () => {
+      const apiClient = new SublinksApiBase();
+
+      await apiClient.Client().getSite();
+
+      // @ts-expect-error Accessing private property
+      expect(apiClient.rawClient.getSite).toHaveBeenCalledTimes(1);
+    });
+
+    it('validates and sets auth when user is logged in but the auth header is not set', async () => {
+      const apiClient = new SublinksApiBase();
+      const cookieStore = {
+        get: jest.fn().mockReturnValue('test-jwt-cookie'),
+        set: jest.fn(),
+        remove: jest.fn()
+      };
+
+      apiClient.setAuthCookieStore(cookieStore);
+
+      await apiClient.login('bill', 'password123');
+      // @ts-expect-error Accessing private property
+      apiClient.rawClient.setHeader('Authorization', undefined);
+
+      // @ts-expect-error Accessing private property
+      expect(apiClient.rawClient.headers.Authorization).toBeUndefined();
+
+      await apiClient.Client().getSite();
+
+      // @ts-expect-error Accessing private property
+      expect(apiClient.rawClient.headers.Authorization).not.toBeUndefined();
+
+      // Once for user auth validation, second time for actual API request
+      // @ts-expect-error Accessing private property
+      expect(apiClient.rawClient.getSite).toHaveBeenCalledTimes(2);
+    });
+
+    it('validates and clears auth on Unauthorized API error', async () => {
+      const apiClient = new SublinksApiBase();
+      const cookieStore = {
+        get: jest.fn(),
+        set: jest.fn(),
+        remove: jest.fn()
+      };
+
+      apiClient.setAuthCookieStore(cookieStore);
+      // @ts-expect-error Accessing private property
+      apiClient.rawClient.setHeader('Authorization', 'test-jwt-cookie');
+
+      try {
+        await apiClient.Client().getPosts();
+      } catch (e) {
+        // Ignoring error
+      }
+
+      // @ts-expect-error Accessing private property
+      expect(apiClient.rawClient.getPosts).toHaveBeenCalled();
+      expect(cookieStore.remove).toHaveBeenCalled();
+    });
+
+    it('throws error on API error', async () => {
+      expect.assertions(1);
+      const apiClient = new SublinksApiBase();
+
+      try {
+        await apiClient.Client().getPosts();
+      } catch (e) {
+        const error = e as Error;
+        expect(error.message).toBe('Unauthorized');
+      }
+    });
   });
 });
