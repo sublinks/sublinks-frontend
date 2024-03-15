ef64c1490b66abbe769c52c6136b6fc5d73f0307
Kenny Gilles <kenny.gilles@gmail.com>
2024-03-15 17:47:40 -0400

    Create separate test projects for client and server

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  projects: [
+    {
+      preset: 'ts-jest',
+      displayName: 'client',
+      testMatch: ["/**/*.test.[jt]s?(x)"],
+      testEnvironment: 'jest-environment-jsdom',
+    },
+    {
+      preset: 'ts-jest',
+      displayName: 'server',
+      testMatch: ["/**/*.server.test.[jt]s?(x)"],
+      testEnvironment: 'node',
+    }
+  ]
 };
