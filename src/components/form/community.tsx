951f8a30a40a9b5d8e2d7d6dba68d6f717069842
Kenny Gilles <kenny.gilles@gmail.com>
2024-04-29 11:07:08 -0600

    Ignore MT error

--- src/components/form/community.tsx
+++ src/components/form/community.tsx
@@ -192,6 +192,8 @@ const CommunityForm = () => {
         {errorMessage && <ErrorText className="text-sm">{errorMessage}</ErrorText>}
       </div>
       <Button type="submit" disabled={isSubmitting} className="flex justify-center">
+        {/*
+        // @ts-expect-error MT isn't up to date with their React types as of 2.1.9 */}
         {isSubmitting ? <Spinner className="h-24 w-24" /> : <BodyTitleInverse>Create Community</BodyTitleInverse>}
       </Button>
     </form>
