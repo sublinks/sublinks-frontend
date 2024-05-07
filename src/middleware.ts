629b7accf904b8f58c28d1abcfb482f600093492
Kenny Gilles <kenny.gilles@gmail.com>
2024-05-07 16:25:58 -0400

    Ignore cookie presence

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,6 @@ import type { NextRequest } from 'next/server';
 import SublinksApi from '@/utils/api-client/server';
 
 export const middleware = async (request: NextRequest) => {
-  const authCookie = SublinksApi.Instance().authCookieStore?.get();
-  if (!authCookie) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-
   const validation = await SublinksApi.Instance().Client().validateAuth();
   if (!validation.success) {
     return NextResponse.redirect(new URL('/login', request.url));
@@ -17,7 +12,7 @@ export const middleware = async (request: NextRequest) => {
   return NextResponse.next();
 };
 
-// Pages that require server-side authentication
+// Pages that require server-side user authentication
 export const config = {
   matcher: ['/p', '/c/create']
 };
