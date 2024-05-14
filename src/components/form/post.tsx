47fd3a3b6bf49568faca7b37ae1eab880748a815
Kenny Gilles <kenny.gilles@gmail.com>
2024-05-14 14:48:22 -0400

    Use request cookies to pre-check auth

--- src/components/form/post.tsx
+++ src/components/form/post.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import React, {
-  FormEvent, useContext, useEffect, useState
-} from 'react';
+import React, { FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { CommunityView } from 'sublinks-js-client';
 import { Spinner } from '@material-tailwind/react';
@@ -13,7 +11,6 @@ import { Selector } from '@/components/input/select';
 import { BodyTitleInverse, ErrorText, PaleBodyText } from '@/components/text';
 import SublinksApi from '@/utils/api-client/client';
 import logger from '@/utils/logger';
-import { UserContext } from '@/context/user';
 import { isImage } from '@/utils/links';
 import { getCommunitySlugFromUrl } from '@/utils/communities';
 
@@ -38,17 +35,10 @@ const REQUIRED_FIELDS = [
 
 const PostForm = ({ communities }: PostFormProps) => {
   const router = useRouter();
-  const { userData } = useContext(UserContext);
   const [erroneousFields, setErroneousFields] = useState<string[]>([]);
   const [errorMessage, setErrorMessage] = useState<string>('');
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isMediaPost, setIsMediaPost] = useState(false);
-
-  useEffect(() => {
-    if (userData.auth === false) {
-      router.push('/login');
-    }
-  }, [userData]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const validateRequiredFields = (fieldValues: Record<string, string | number | File>) => {
     const emptyFields: string[] = [];
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+import { AUTH_COOKIE_NAME } from '@/utils/api-client/base';
 import SublinksApi from '@/utils/api-client/server';
 
 export const middleware = async (request: NextRequest) => {
+  const authCookie = request.cookies.get(AUTH_COOKIE_NAME);
+  if (!authCookie) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
   const validation = await SublinksApi.Instance().Client().validateAuth();
   if (!validation.success) {
     return NextResponse.redirect(new URL('/login', request.url));
