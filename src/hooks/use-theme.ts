874ad9cbfc1dafc839c5baf840be197804693036
Billy Matthews <billy.matthews@bucknell.edu>
2024-06-06 10:47:26 -0600

    Fix eslint issues

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -1,21 +1,20 @@
 'use client';
 
-type Theme = 'light' | 'dark';
-
 import { useLocalStorage } from '@/hooks/use-local-storage';
 import { handleSaveUserSettings } from '@/utils/settings';
 
-export function useTheme(initialTheme?: Theme ) {
-   initialTheme = initialTheme ?? (typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+type Theme = 'light' | 'dark';
 
-   const [theme, setTheme] = useLocalStorage('theme', initialTheme);
+export function useTheme(initialTheme?: Theme) {
+  const originalTheme = initialTheme ?? (typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
-   const saveTheme = async (newTheme) => {
-      const oldTheme = theme;
-      setTheme(newTheme);
-      
-      const newUserSettings = await handleSaveUserSettings({ theme: newTheme });
-   }
+  const [theme, setTheme] = useLocalStorage('theme', originalTheme);
 
-   return [theme, saveTheme];
+  const saveTheme = async newTheme => {
+    setTheme(newTheme);
+
+    await handleSaveUserSettings({ theme: newTheme });
+  };
+
+  return [theme, saveTheme];
 }
--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -3,11 +3,10 @@ import { SaveUserSettings } from 'sublinks-js-client';
 import SublinksApi from './api-client/client';
 import logger from './logger';
 
-export const handleSaveUserSettings = async ( userSettings: SaveUserSettings) => {
-
-   try {
-      const updatedUserSettings = await SublinksApi.Instance().Client().saveUserSettings(userSettings);
-   } catch (e) {
-      logger.error(`Failed to save user settings ${userSettings}`, e);
-   }
+export const handleSaveUserSettings = async (userSettings: SaveUserSettings) => {
+  try {
+    await SublinksApi.Instance().Client().saveUserSettings(userSettings);
+  } catch (e) {
+    logger.error(`Failed to save user settings ${userSettings}`, e);
+  }
 };
