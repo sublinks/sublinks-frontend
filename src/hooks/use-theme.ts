9e2d695024668536d5f05ebdee37769795dbc361
Billy Matthews <the.number.8@gmail.com>
2024-06-11 16:37:02 -0600

    Issue/62 (#166)
    * Updated to address issue 62 -- connecting the theme switch UI to their user setting in the db via the sublinks client api
    
    * Updated to address issue 62 load the initial theme value from the DB if present
    
    * Fix eslint issues
    
    * Updated layout to check for initial theme
    
    * Addressed type issues

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -1,7 +1,20 @@
 'use client';
 
 import { useLocalStorage } from '@/hooks/use-local-storage';
+import { handleSaveUserSettings } from '@/utils/settings';
 
-export function useTheme() {
-  return useLocalStorage('theme', typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+export type Theme = 'light' | 'dark';
+
+export function useTheme(initialTheme: Theme): [Theme, (theme: Theme) => void] {
+  const originalTheme = initialTheme ?? (typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+
+  const [theme, setTheme] = useLocalStorage('theme', originalTheme);
+
+  const saveTheme = async (newTheme: Theme) => {
+    setTheme(newTheme);
+
+    await handleSaveUserSettings({ theme: newTheme });
+  };
+
+  return [theme, saveTheme];
 }
