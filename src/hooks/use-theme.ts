341f12c3281a835f0732fbe087fbf84c238e1ce1
Billy Matthews <billy.matthews@bucknell.edu>
2024-06-11 14:56:01 -0600

    Addressed type issues

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -3,14 +3,14 @@
 import { useLocalStorage } from '@/hooks/use-local-storage';
 import { handleSaveUserSettings } from '@/utils/settings';
 
-type Theme = 'light' | 'dark';
+export type Theme = 'light' | 'dark';
 
-export function useTheme(initialTheme?: Theme) {
+export function useTheme(initialTheme: Theme): [Theme, (theme: Theme) => void] {
   const originalTheme = initialTheme ?? (typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
   const [theme, setTheme] = useLocalStorage('theme', originalTheme);
 
-  const saveTheme = async newTheme => {
+  const saveTheme = async (newTheme: Theme) => {
     setTheme(newTheme);
 
     await handleSaveUserSettings({ theme: newTheme });
